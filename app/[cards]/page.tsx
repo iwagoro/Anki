8780b80b8735165cbf7462ee35f3aa6aeb922400
ROCKWELL <rockwell@ROCKWELLnoMacBook-Air.local>
2024-04-07 11:13:49 -0700

    fixed wordcord1

--- app/[cards]/page.tsx
+++ app/[cards]/page.tsx
@@ -76,7 +76,7 @@ export default function Home() {
                 <div {...handlers} className=" pt-[70px] max-w-md w-full h-full flex flex-col pt-50  px-10 justify-between gap-5">
                     <Progress value={((index + 1) * 100) / words.length}></Progress>
                     {show && words[index] && <WordCard1 word={words[index].word} definition={words[index].definition} forgot={words[index].forgot} change={index}></WordCard1>}
-                    {!show && <WordCard1 word="" definition="" forgot={words[index].forgot} change={index}></WordCard1>}
+                    {!show && <WordCard1 word="" definition="" forgot={false} change={index}></WordCard1>}
                     <div className="w-full h-[100px] flex items-center">
                         <Button
                             className="max-w-[50%] w-full h-full gap-3 items-center"
