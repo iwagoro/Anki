4a74159e79ff76db696068b761a1c2e49096b203
ROCKWELL <rockwell@ROCKWELLnoMacBook-Air.local>
2024-04-10 22:02:49 -0700

    a

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,29 @@
 "use client";
-import { use, useEffect } from "react";
+import { use, useEffect, useContext } from "react";
+import { AppContext } from "@/components/util/provider";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { app } from "@/components/util/firebase";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
+    const { user, setUser } = useContext(AppContext);
     const router = useRouter();
-    useEffect(() => {
-        router.push("/home");
-    }, []);
 
+    useEffect(() => {
+        const auth = getAuth(app);
+        const fetchData = async () => {
+            const unsubscribe = onAuthStateChanged(auth, async (user) => {
+                if (user) {
+                    setUser(user.email || "");
+                } else {
+                    setUser("");
+                    router.push("/login");
+                }
+            });
+            return unsubscribe;
+        };
+        fetchData();
+    }, []);
     return <></>;
 }
 
