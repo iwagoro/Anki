f4a449576e3f4a5c0efb1a1144fb5d0e4d332231
ROCKWELL <rockwell@ROCKWELLnoMacBook-Air.local>
2024-04-10 22:01:09 -0700

    a

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,37 @@
 "use client";
-import { use, useEffect } from "react";
+import { use, useEffect, useContext } from "react";
+import { AppContext } from "@/components/util/provider";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { app } from "@/components/util/firebase";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
+<<<<<<< Updated upstream
     const router = useRouter();
     useEffect(() => {
         router.push("/home");
     }, []);
 
+=======
+    const { user, setUser } = useContext(AppContext);
+    const router = useRouter();
+
+    useEffect(() => {
+        const auth = getAuth(app);
+        const fetchData = async () => {
+            const unsubscribe = onAuthStateChanged(auth, async (user) => {
+                if (user) {
+                    setUser(user.email || "");
+                } else {
+                    setUser("");
+                    router.push("/login");
+                }
+            });
+            return unsubscribe;
+        };
+        fetchData();
+    }, []);
+>>>>>>> Stashed changes
     return <></>;
 }
 
